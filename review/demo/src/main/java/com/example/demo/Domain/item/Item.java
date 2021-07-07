d11926c37f64c26b7e8400175380ddf79d5e2937
cjlee38 <gptpem38@gmail.com>
2021-07-07 23:44:29 +0900

    ADD : 상품 도메인 개발

--- review/demo/src/main/java/com/example/demo/Domain/item/Item.java
+++ review/demo/src/main/java/com/example/demo/Domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.example.demo.Domain.item;
 
 import com.example.demo.Domain.Category;
+import com.example.demo.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,6 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 한 테이블에 다 때려박는것...?
 @DiscriminatorColumn(name = "dtype")
 @Getter
-@Setter
 public abstract class Item {
 
     @Id
@@ -20,7 +20,7 @@ public abstract class Item {
     @Column(name = "item_id")
     private Long id;
 
-    private String name;
+    private String name;명
     private int price;
     private int stockQuantity;
 
@@ -28,4 +28,25 @@ public abstract class Item {
     private List<Category> categories = new ArrayList<>();
 
 
+    // == 비즈니스 로직 == //
+
+    /**
+     * stock 증가
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     * @param quantity
+     */
+    public void subStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("재고가 부족합니다.");
+        }
+        this.stockQuantity = restStock;
+    }
 }
---
+++ review/demo/src/main/java/com/example/demo/Repository/ItemRepository.java
@@ -0,0 +1,36 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    @PersistenceContext
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item); // 기존에 item이 없다 -> 아이템을 처음 등록할때는 아직 id가 없음.
+        } else {
+            em.merge(item); // 업데이트 비슷한것. -> 디비에 이미 있는 녀석을 변경하겠다. (나중에 설명)
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+
+
+}
